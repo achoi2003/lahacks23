d5b2285bb095c0505359f42340c5bb6771c7bf69
Andy Huang <andyhuang715@gmail.com>
2023-04-22 21:19:09 -0700

    front_end_template

--- course-planner/my-app/src/App.js
+++ course-planner/my-app/src/App.js
@@ -2,26 +2,94 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const people = ['kevin', 'andy', 'alex']
-
+  const people = ['kevin', 'andy', 'alex'];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <ul>
-          {people.map(p => <li>{p}</li>)}
-        </ul>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class = "title"> <h1> College Degree Planner</h1> </div>
+
+        {/* start of the Academic Year */}
+        <div class = "header"> <h3> Year 1 </h3></div>
+        <div class="flex-container">
+        
+        {/* One Quarter */}
+        <div> 
+          <div> <h3> Fall </h3> </div>
+          <div class = "flex-table">
+            <div> <input type="text"/> </div>
+            <div> <input type="text"/> </div>
+            <div> <input type="text"/> </div>
+            <div> <input type="text"/> </div>
+            <div> class 2</div>
+          </div>
+         </div>
+
+         <div> 
+          <div> <h3> Winter </h3> </div>
+          <div class = "flex-table">
+            <div> <input type="text"/> </div>
+            <div> <input type="text"/> </div>
+            <div> <input type="text"/> </div>
+            <div> <input type="text"/> </div>
+            <div> class 2</div>
+          </div>
+         </div>
+
+        <div> 
+          <div> <h3> Spring </h3> </div>
+          <div class = "flex-table">
+            <div> <input type="text"/> </div>
+            <div> <input type="text"/> </div>
+            <div> <input type="text"/> </div>
+            <div> <input type="text"/> </div>
+            <div> class 2</div>
+          </div>
+         </div> 
+
+        <div> 
+          <div> <h3> Summer </h3> </div>
+          <div class = "flex-table">
+            <div> <input type="text"/> </div>
+            <div> <input type="text"/> </div>
+            <div> <input type="text"/> </div>
+            <div> <input type="text"/> </div>
+            <div> class 2</div>
+          </div>
+         </div> 
+
+        </div>
+
+
+
+        <div class = "header"> <h3> Year 2 </h3></div>
+        <div class="flex-container">
+        <div> 
+          <div class = "flex-table">
+            <div> <input type="text"/> </div>
+            <div> <input type="text"/> </div>
+            <div> <input type="text"/> </div>
+            <div> <input type="text"/> </div>
+            <div> class 2</div>
+          </div>
+         </div>
+        <div> Winter </div>
+        <div> Spring </div>
+        </div>
+
+        <div class = "header"> <h3> Year 3  </h3></div>
+        <div class="flex-container">
+        <div> Fall </div>
+        <div > Winter </div>
+        <div> Spring </div>
+        </div>
+
+        <div class = "header"> <h3> Year 4  </h3></div>
+        <div class="flex-container">
+        <div> Fall</div>
+        <div > Winter </div>
+        <div> Spring </div>
+        </div>
+
       </header>
     </div>
   );
