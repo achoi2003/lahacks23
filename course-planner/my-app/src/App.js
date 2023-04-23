b65e5ad3c4627e0c8ae4bb5ee39192e6f21e8249
Alex Choi <achoi2003@gmail.com>
2023-04-23 01:57:49 -0700

    An honest day's work

--- course-planner/my-app/src/App.js
+++ course-planner/my-app/src/App.js
@@ -1,246 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
-import React, {useState} from 'react';
-
-// function NumberInput() {
-//   const [number, setNumber] = useState('');
-
-//   function handleChange(event) {
-//     setNumber(event.target.value);
-//   }
-
-//   return (
-//     <div>
-//         <input type="number" value={number} onChange={handleChange} />
-//       <p>The number you entered is: {number}</p>
-//     </div>
-//   );
-//   }
-
+import React, {useState} from "react";
+import Table from "./Table";
 
 function App() {
+  // quarter(0) or semester(1)?
+  const [quarters, setQuarters] = useState(0);
+  // summer(0) or no(1)?
+  const [summer, setSummer] = useState(0);
+  // 3, 4, or 5 years?
+  const [years, setYears] = useState(3); 
+
+  // get the value of the selected option 
+  // dynamically update the state variable
+function handleQuartersChange(event) {
+  const value = parseInt(event.target.value);
+  console.log('New quarters value:', value);
+  setQuarters(value);
+  // setQuarters(Number(event.target.value));
+}
+function handleSummerChange(event) {
+  const value = parseInt(event.target.value);
+  console.log('New summer value:', value);
+  setSummer(value);
+  // setSummer(Number(event.target.value));
+}
+function handleYearsChange(event) {
+  const value = parseInt(event.target.value);
+  console.log('New years value:', value);
+  setYears(value);
+  // setYears(Number(event.target.value));
+}
   return (
     <div className="App">
       <header className="App-header">
-        <div class = "title"> <h2> College Degree Planner</h2> </div>
+      <div className = "title"> <h2> College Degree Planner</h2> </div>
 
-        {/* start of the Academic Year */}
-        <div class = "header"> <h5> Year 1 </h5></div>
-        <div class="flex-container">
-        
-        {/* One Quarter */}
-        <div> 
-          <div class = "flex-table">
-            <div> <h4> Fall </h4> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-          </div>
-         </div>
+<p style={{ marginTop: '0px' }}>
+        <label className="dropdown-label" style={{ marginTop: '0' }}>
+        Quarter or Semester System:
+        <select value={quarters} onChange={handleQuartersChange}>
+          <option value="0">Quarter</option>
+          <option value="1">Semester</option>
+        </select>
+      </label>
+          
+      <label className="dropdown-label">
+        Include Summer:
+        <select value={summer} onChange={handleSummerChange}>
+          <option value="0">Yes</option>
+          <option value="1">No</option>
+        </select>
+      </label>
+  
+      <label className="dropdown-label">
+      3, 4 or 5 years:
+        <select value={years} onChange={handleYearsChange}>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+      </label>
+      </p>
 
-         <div> 
-          <div class = "flex-table">
-          <div> <h4> Winter </h4> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-          </div>
-         </div>
-
-        <div> 
-          <div class = "flex-table">
-          <div> <h4> Spring </h4> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-          </div>
-         </div> 
-
-        <div> 
-          <div class = "flex-table">
-          <div> <h4> Summer </h4> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-          </div>
-         </div> 
-        </div>
-
-
-        <div class = "header"> <h5> Year 2 </h5></div>
-        <div class="flex-container">
-        
-        {/* One Quarter */}
-        <div> 
-          <div class = "flex-table">
-            <div> <h4> Fall </h4> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-          </div>
-         </div>
-
-         <div> 
-          <div class = "flex-table">
-          <div> <h4> Winter </h4> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-          </div>
-         </div>
-
-        <div> 
-          <div class = "flex-table">
-          <div> <h4> Spring </h4> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-          </div>
-         </div> 
-
-        <div> 
-          <div class = "flex-table">
-          <div> <h4> Summer </h4> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-          </div>
-         </div> 
-        </div>
-
-
-        <div class = "header"> <h5> Year 3 </h5></div>
-        <div class="flex-container">
-        
-        {/* One Quarter */}
-        <div> 
-          <div class = "flex-table">
-            <div> <h4> Fall </h4> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-          </div>
-         </div>
-
-         <div> 
-          <div class = "flex-table">
-          <div> <h4> Winter </h4> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-          </div>
-         </div>
-
-        <div> 
-          <div class = "flex-table">
-          <div> <h4> Spring </h4> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-          </div>
-         </div> 
-
-        <div> 
-          <div class = "flex-table">
-          <div> <h4> Summer </h4> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-          </div>
-         </div> 
-        </div>
-
-
-        <div class = "header"> <h5> Year 4 </h5></div>
-        <div class="flex-container">
-        
-        {/* One Quarter */}
-        <div> 
-          <div class = "flex-table">
-            <div> <h4> Fall </h4> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-          </div>
-         </div>
-
-         <div> 
-          <div class = "flex-table">
-          <div> <h4> Winter </h4> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-          </div>
-         </div>
-
-        <div> 
-          <div class = "flex-table">
-          <div> <h4> Spring </h4> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-          </div>
-         </div> 
-
-        <div> 
-          <div class = "flex-table">
-          <div> <h4> Summer </h4> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-            <div> <input type="text"/> </div>
-          </div>
-         </div> 
-        </div>
-
-
+      <Table quarters={quarters} summer={summer} years={years} 
+      />
       </header>
     </div>
   );
